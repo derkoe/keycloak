bbe1fcb50db2083032009504f68ad68cb4a4f170
Christian Köberl <derkoe@users.noreply.github.com>
2023-04-14 08:42:03 +0000

    Add bcrypt password hashing provider

---
+++ server-spi-private/src/main/java/org/keycloak/credential/hash/BCryptPasswordHashProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.credential.hash;
+
+import org.keycloak.models.PasswordPolicy;
+import org.keycloak.models.credential.PasswordCredentialModel;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class BCryptPasswordHashProvider implements PasswordHashProvider {
+    private final int defaultIterations;
+    private final String providerId;
+
+    public BCryptPasswordHashProvider(String providerId, int defaultIterations) {
+        this.providerId = providerId;
+        this.defaultIterations = defaultIterations;
+    }
+
+    @Override
+    public boolean policyCheck(PasswordPolicy policy, PasswordCredentialModel credential) {
+        int policyHashIterations = policy.getHashIterations();
+        if (policyHashIterations == -1) {
+            policyHashIterations = defaultIterations;
+        }
+
+        return credential.getPasswordCredentialData().getHashIterations() == policyHashIterations
+            && providerId.equals(credential.getPasswordCredentialData().getAlgorithm());
+    }
+
+    @Override
+    public PasswordCredentialModel encodedCredential(String rawPassword, int iterations) {
+        String encodedPassword = encode(rawPassword, iterations);
+
+        // bcrypt salt is stored as part of the encoded password so no need to store salt separately
+        return PasswordCredentialModel.createFromValues(providerId, new byte[0], iterations, encodedPassword);
+    }
+
+    @Override
+    public String encode(String rawPassword, int iterations)
+    {
+        PasswordEncoder encoder = new BCryptPasswordEncoder(iterations);
+        return encoder.encode(rawPassword);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public boolean verify(String rawPassword, PasswordCredentialModel credential)
+    {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.matches(rawPassword, credential.getPasswordSecretData().getValue());
+    }
+}
